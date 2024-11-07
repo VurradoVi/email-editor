5d36e87db6abf086c98ff0f57f58f783034654a1
Vlad <02kvg03@mail.ru>
2024-11-07 19:42:01 +0300

    applyStyle

--- src/components/email-editor/EmailEditor.tsx
+++ src/components/email-editor/EmailEditor.tsx
@@ -1,28 +1,79 @@
 import { Bold, Eraser, Italic, Underline } from "lucide-react";
 import styles from "./EmailEditor.module.scss";
-import React from "react";
+import React, { useRef, useState } from "react";
+import { applyStyle, TStyle } from "./apply-style";
+
 
 const tools = [
   { component: <Eraser /> },
   { component: <Bold /> },
-  { component: <Italic />},
-  { component: <Underline />},
+  { component: <Italic /> },
+  { component: <Underline /> },
 ];
 
 export function EmailEditor() {
+  const [text, setText] = useState("loreMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+  const textRef = useRef<HTMLTextAreaElement | null>(null);
+  const [selectionStart, setSelectionStart] = useState(0)
+  const [selectionEnd, setSelectionEnd] = useState(0)
+
+  const updateSelection = () => {
+    if (!textRef.current) return
+    setSelectionStart(textRef.current.selectionStart)
+    setSelectionEnd(textRef.current.selectionEnd)
+  } 
+
+  const applyFormat = (type: TStyle) => {
+    const selectedText = text.substring(selectionStart, selectionEnd)
+    
+    if (!selectedText) return
+    const before = text.substring(0, selectionStart)
+    const after = text.substring(selectionEnd)
+
+    setText(before + applyStyle(type, selectedText) + after)
+    
+  };
+
+  const handleClick = (index) => {
+    switch(index) {
+      case 0:
+        setText('')
+        break;
+      case 1:
+        applyFormat('bold')
+        break;
+      case 2:
+        applyFormat('italic')
+        break;
+      case 1:
+        applyFormat('underline')
+        break;
+      default:
+        console.log(`Кнопка ${index} нажата`);
+    }
+  };
+
   return (
     <div>
       <h1>Email editor</h1>
       <div className={styles.card}>
-        <div className={styles.editor}>
-          hey! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia
-          ex voluptates esse, doloremque sequi officiis rerum quibusdam eveniet
-          deserunt distinctio numquam reiciendis impedit odit cupiditate
-          repellat error explicabo natus perspiciatis?
-        </div>
+        <textarea
+          ref={textRef}
+          className={styles.editor}
+          spellCheck="false"
+          onSelect={updateSelection}
+          value={text}
+          onChange={e => setText(e.target.value)}
+        >
+          {text}
+        </textarea>
         <div className={styles.actions}>
           <div className={styles.tools}>
-            {tools.map((t, i) => <button key={i}>{React.cloneElement(t.component, { size: 18 })}</button>)}
+            {tools.map((t, i) => (
+              <button key={i} onClick={() => handleClick(i)}>
+                {React.cloneElement(t.component, { size: 18 })}
+              </button>
+            ))}
           </div>
           <button>Send</button>
         </div>
--- src/components/email-list/EmailList.tsx
+++ src/components/email-list/EmailList.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import style from "./emailList.module.scss";
 
 export default function EmailList() {
   return (
-    <div>
-      <p>EmailList</p>
+    <div className={style.list}>
+      <div>
+        hey! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia
+        ex voluptates esse, doloremque sequi officiis rerum quibusdam eveniet
+        deserunt distinctio numquam reiciendis impedit odit cupiditate repellat
+        error explicabo natus perspiciatis?
+      </div>
     </div>
-  )
+  );
 }
